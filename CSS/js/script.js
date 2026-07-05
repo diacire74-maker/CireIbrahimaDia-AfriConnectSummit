17605fdbbcd6eb33c2d0a19fd078b2e67c76ac18
CireIbrahimaDia-AfriConnectSummit <diacire74@gmail.com>
2026-07-05 04:09:24 +0000

    Ajout de la section Hero avec compte à rebours

--- CSS/js/script.js
+++ CSS/js/script.js
@@ -2,3 +2,40 @@
 
 document.getElementById("year").textContent =
 new Date().getFullYear();
+/* ===========================
+Compte à rebours
+=========================== */
+
+const targetDate = new Date("November 15, 2026 09:00:00").getTime();
+
+function updateCountdown() {
+
+    const now = new Date().getTime();
+
+    const distance = targetDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    if(document.getElementById("days")){
+
+        document.getElementById("days").textContent = days;
+
+        document.getElementById("hours").textContent = hours;
+
+        document.getElementById("minutes").textContent = minutes;
+
+        document.getElementById("seconds").textContent = seconds;
+
+    }
+
+}
+
+setInterval(updateCountdown,1000);
+
+updateCountdown();
